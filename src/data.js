5fe2b7dcddd2bbe1cb95c39301296d2293e6237d
Akash9363 <aksingh9363@gmail.com>
2025-03-26 03:01:02 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const reviews=[
+    {
+        id:1,
+        name: "Sundar Pichai",
+        job: "Software Engineer",
+        image:"Sundar_Pichai.jpg",
+        text:"As an engineer, my passion lies in creating solutions that bridge theory and practical application, ensuring every project meets the highest standards of excellence. Driven by curiosity and backed by experience, I approach each challenge with a mindset focused on innovation and efficiency." ,
+    },
+    {
+        id:2,
+        name: "Elon Musk",
+        job: "Graphic Designer",
+        image:"Elon_Musk.jpg",
+        text:"With every design and build, I strive to combine precision, creativity, and sustainability to push the boundaries of engineering. My career as an engineer is rooted in a commitment to continuous learning and delivering results that make a tangible impact.",
+    },
+    {
+        id:3,
+        name: "Ratan Tata",
+        job: "Marketing Manager",
+        image:"Ratan_Tata.jpeg",
+        text:"Engineering is not just my profession but my platform for solving real-world problems and contributing to a more connected, sustainable world. I believe that great engineering is about finding practical, elegant solutions to complex challenges, balancing creativity with function.",
+    },
+    {
+        id:4,
+        name: "Jeff Beozs",
+        job: "Content Strategist",
+        image:"Jeff_Bezos.jpg",
+        text:"Through my work, I aim to blend technical expertise with a collaborative approach to achieve outcomes that surpass expectations. The true mark of an engineer's success lies in the harmony between technical prowess and practical, user-focused design—and that's my goal every day."
+    },
+    {
+        id:5,
+        name: "Mark Zucerberg",
+        job: "Data Analyst",
+        image:"Mark_Berg.jpg",
+        text:"Engineering has taught me that every problem has a solution, and every solution is an opportunity to learn, grow, and innovate. Every project I undertake reflects my dedication to quality, precision, and the pursuit of engineering excellence.",
+    },
+
+]
+
+export default reviews;      
